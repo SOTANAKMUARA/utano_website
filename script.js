1ef55528a6a3e729579e5d13c352050f2a4b7c89
SOTANAKMUARA <50124128+SOTANAKMUARA@users.noreply.github.com>
2020-08-24 21:40:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ document.addEventListener('DOMContentLoaded', function(){
 		document.getElementById('nav').classList.toggle("active");
 	})
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota1').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota2').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota3').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota4').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('sota5').addEventListener("click", function() {
+		document.getElementById('nav').classList.toggle("active");
+		// document.getElementById('nav').classList.toggle("active");
+	})
+});
